da29271ce36927617c342e1d00731d4c92d5dc6c
David Richards <35054853+ravenOSS@users.noreply.github.com>
2021-04-28 09:53:45 -0600

    remove external CSS. Replace with jsx

--- pages/profile.js
+++ pages/profile.js
@@ -1,10 +1,12 @@
 import Layout from '../components/Layout'
-import styles from '../styles/Profile.module.css'
+// import styles from '../styles/Profile.module.css'
 
 export default function Profile() {
 	return (
 		<Layout>
-			<h1>This is the Profile Page</h1>
+			<div>
+				<h1>This is the Profile Page</h1>
+			</div>
 			<style jsx>{`
 				h1,
 				div {
