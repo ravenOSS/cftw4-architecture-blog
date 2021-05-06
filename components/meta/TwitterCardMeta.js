d43d0acc99f46f94bc52016d5c35a401afba30d9
David Richards <35054853+ravenOSS@users.noreply.github.com>
2021-05-06 17:15:07 -0600

    Cleanup Twitter Card Meta

--- components/meta/TwitterCardMeta.js
+++ components/meta/TwitterCardMeta.js
@@ -4,9 +4,9 @@ import config from '../../config.json'
 export default function TwitterCardMeta({ url, title, description }) {
 	return (
 		<Head>
-			<meta property='twitter:card' content='summary_large_image' />
+			<meta property='twitter:card' content='summary' />
 			<meta property='twitter:site' content={config.twitter_account} />
-			<meta property='twitter:url' content={config.base_url + url} />
+			<meta property='twitter:url' content={config.base_url} />
 			<meta property='twitter:title' content={config.site_title} />
 			<meta property='twitter:creator' content={config.twitter_account} />
 			<meta property='twitter:description' content={config.site_description} />
